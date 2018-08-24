4ed6d55a81e46af7951a06515334599ccc72ef2a
DESKTOP-IADFLHJ\Luis <triskel67@hotmail.com>
2018-08-25 00:23:50 +0200

    Fix kingdom borders

--- src/com/luis/strategy/datapackage/scene/ArmyData.java
+++ src/com/luis/strategy/datapackage/scene/ArmyData.java
@@ -3,28 +3,38 @@ package com.luis.strategy.datapackage.scene;
 import java.io.Serializable;
 import java.util.List;
 
-public class ArmyData implements Serializable{
-	
-	private static final long serialVersionUID = 1L;
-	
-	private List<TroopData> troopList;
-	private KingdomData kingdom;
+public class ArmyData implements Serializable {
 
-	public List<TroopData> getTroopList() {
-		return troopList;
-	}
+    private static final long serialVersionUID = 1L;
 
-	public void setTroopList(List<TroopData> troopList) {
-		this.troopList = troopList;
-	}
+    private List<TroopData> troopList;
+    private KingdomData kingdom;
+    private long seed;
 
-	public KingdomData getKingdom() {
-		return kingdom;
-	}
+    public List<TroopData> getTroopList() {
+        return troopList;
+    }
 
-	public void setKingdom(KingdomData kingdom) {
-		this.kingdom = kingdom;
-	}
+    public void setTroopList(List<TroopData> troopList) {
+        this.troopList = troopList;
+    }
 
-	
+    public KingdomData getKingdom() {
+        return kingdom;
+    }
+
+    public void setKingdom(KingdomData kingdom) {
+        this.kingdom = kingdom;
+    }
+
+    public long getSeed() {
+        return seed;
+    }
+
+    public void setSeed(long seed) {
+        this.seed = seed;
+    }
+
+    
+
 }
